3a7dadbef729ba575a67050deda82f201b1a84d3
maciej.smolczewski <mSmolczewski@gmail.com>
2024-06-23 11:07:44 +0200

    add test tournament script

---
+++ testData/testNextMagog.bat
@@ -0,0 +1,18 @@
+:: Each opening as black and white against each player. After playing the opening with every player go to next opening. 
+:: Set number of rounds to number of openings!
+
+IF [%1] == [] ( 
+	ECHO Tournament name missing
+	goto :eof
+)
+
+cutechess-cli -tournament gauntlet -openings file=basicOpenings.pgn policy=round -debug ^
+-resign movecount=5 score=1400 twosided=true ^
+-maxmoves 300 ^
+-concurrency 2 ^
+-pgnout %1"games.pgn" -event %1 ^
+-engine conf=magog.Challenger ^
+-engine conf=magog.Defender ^
+-engine conf=DoctorB ^
+-engine conf=EnkoChess_290818 ^
+-each tc=120+1 -games 2 -rounds 7 -repeat > %1".debug"
